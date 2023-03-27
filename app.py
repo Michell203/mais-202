b0fc68226b49c8829ac83969b312ae1aa4a6e30d
Michel Hijazin <michaelhijazeen203@gmail.com>
2023-03-27 13:27:15 -0400

    Added saved model and utilities.py file

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request
 #import MusiClassifier
-#import tensorflow as tf
+import tensorflow as tf
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
 @app.route("/data", methods=["POST"])
 def data():
-    #model  = tf.keras.models.load_model('./Data/')
+    model  = tf.keras.models.load_model('./saved_model')
     genre = "Some genre"#MusiClassifier.predictFromAudio(data)
     #print(type(request.data))
     return genre
+
+if __name__ == "__main__":
+    app.run()
