bef773ca8be530687d915510cef20c37f388c970
spynob <simon.pino.buisson103@gmail.com>
2023-03-29 20:29:21 -0400

    Transformation: work in progress; predict: should work

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import wave
 from flask import Flask, render_template, request
 import keras
+import numpy as np
 import tensorflow as tf
+import librosa
+from pydub import AudioSegment
+import math
 
 app = Flask(__name__)
 #model should probably be an attribute
@@ -12,20 +17,32 @@ def index():
 @app.route("/data", methods=["POST"])
 def data():
     model = tf.keras.models.load_model('./saved_model')
-    song = transform(request.data)
-    genre = predict(song, model)
+    songFeatures = transform(request.data)
+    genre = predict(model, songFeatures)
     print(genre)
     return {"genre":genre}
 
 def transform(audioFile):
     #TODO:
     # Transform the .wav file into unsable array of features
+    segmentedList = segment(audioFile)
+    print(segmentedList.size)
+
     return None
 
-def predict(song, model):
-    #TODO:
-    # use the model parameter and return the genre using the array of the song's features
-    return None
+def predict(model,X):
+
+    X=X[np.newaxis,...]
+    
+    prediction=model.predict(X)
+
+
+    #get index with max value
+    predictIndex=np.argmax(prediction, axis=1)
+
+    print(predictIndex)
+    genres = ["Blues", "Classical", "Country", "Disco", "Hip-Hop", "Jazz", "Metal", "Pop", "Reggae", "Rock"]
+    return genres[predictIndex]
 
 def createModel():
     model = keras.models.Sequential([
@@ -53,3 +70,26 @@ def createModel():
     model.load_weights(fname)
     
     return model
+
+def segment(audiofile):
+    segmentduration = 3000 #milliseconds
+    segmentedList = np.array([])
+    audio = AudioSegment.from_wav(audiofile)
+    duration = math.floor(audio.duration_seconds * 1000)
+    print(duration)
+    t1 = 0
+    t2 = t1 + segmentduration
+    print(audio[t1:t2])
+    while t2 < duration:
+        segmentedList = np.append(segmentedList, segment)
+        print(segmentedList)
+        t1 = t1 + segmentduration
+        t2 = t2 + segmentduration
+    
+    segmentedList = np.append(segmentedList, audio[t1:duration])
+
+    return segmentedList
+
+
+audio = "../Her Majesty (Remastered 2009).wav"
+transform(audio)
