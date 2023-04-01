6d2d248e14df40cad62e657a97e3adb21cbc0adc
Michel Hijazin <michaelhijazeen203@gmail.com>
2023-04-01 15:52:09 -0400

    small changes to port

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import tensorflow as tf
 import os
 import librosa
+import pandas as pd
 # from pydub import AudioSegment
 import math
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 #model should probably be an attribute
 
 # reconstructed_model = tf.keras.models.load_model("my_model_h5.h5")
-model_features = ["chroma_stft_mean",	"chroma_stft_var",	"rms_mean",	"rms_var",	"spectral_centroid_mean",	"spectral_centroid_var",	"spectral_bandwidth_mean",	"spectral_bandwidth_var",	"rolloff_mean",	"rolloff_var",	"zero_crossing_rate_mean",	"zero_crossing_rate_var",	"harmony_mean",	"harmony_var",	"perceptr_mean",	"perceptr_var",	"tempo",	"mfcc1_mean",	"mfcc1_var",	"mfcc2_mean",	"mfcc2_var",	"mfcc3_mean",	"mfcc3_var",	"mfcc4_mean",	"mfcc4_var",	"mfcc5_mean",	"mfcc5_var",	"mfcc6_mean",	"mfcc6_var",	"mfcc7_mean",	"mfcc7_var",	"mfcc8_mean",	"mfcc8_var",	"mfcc9_mean",	"mfcc9_var",	"mfcc10_mean",	"mfcc10_var",	"mfcc11_mean",	"mfcc11_var",	"mfcc12_mean",	"mfcc12_var",	"mfcc13_mean",	"mfcc13_var",	"mfcc14_mean",	"mfcc14_var",	"mfcc15_mean",	"mfcc15_var",	"mfcc16_mean",	"mfcc16_var",	"mfcc17_mean",	"mfcc17_var",	"mfcc18_mean",	"mfcc18_var",	"mfcc19_mean",	"mfcc19_var",	"mfcc20_mean",	"mfcc20_var"]
+model_features = ["chroma_stft_mean","chroma_stft_var","rms_mean","rms_var","spectral_centroid_mean","spectral_centroid_var","spectral_bandwidth_mean","spectral_bandwidth_var","rolloff_mean","rolloff_var","zero_crossing_rate_mean","zero_crossing_rate_var","harmony_mean","harmony_var","perceptr_mean","perceptr_var","tempo","mfcc1_mean","mfcc1_var","mfcc2_mean","mfcc2_var","mfcc3_mean","mfcc3_var","mfcc4_mean","mfcc4_var","mfcc5_mean","mfcc5_var","mfcc6_mean","mfcc6_var","mfcc7_mean","mfcc7_var","mfcc8_mean","mfcc8_var","mfcc9_mean","mfcc9_var","mfcc10_mean","mfcc10_var","mfcc11_mean","mfcc11_var","mfcc12_mean","mfcc12_var","mfcc13_mean","mfcc13_var","mfcc14_mean","mfcc14_var","mfcc15_mean","mfcc15_var","mfcc16_mean","mfcc16_var","mfcc17_mean","mfcc17_var","mfcc18_mean","mfcc18_var","mfcc19_mean","mfcc19_var","mfcc20_mean","mfcc20_var"]
 
 @app.route("/")
 def index():
@@ -55,14 +56,19 @@ def transform(audioFile):
         # Extract zero-crossing rate
         zcr = librosa.feature.zero_crossing_rate(y=segment)
         # Concatenate all features
-        feature_vector = np.concatenate(mfccs.flatten(), cent.flatten(), zcr.flatten())
+        feature_vector = np.concatenate((mfccs.flatten(), cent.flatten(), zcr.flatten()))
 
 
         features.append(feature_vector)
-        features = features[model_features]
+        animals = pd.DataFrame(features,model_features)
+        animals.to_csv("file.csv")
+        # features = np.array(features)
+        feature_data = pd.read_csv("file.csv")
+        # features = features[model_features]
+        features = feature_data[model_features]
 
     # Convert the feature list to a numpy array
-    features = np.array(features)
+    # features = np.array(features)
     return features
 
 def predict(model,X):
@@ -107,12 +113,12 @@ def createModel():
     return model
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(host=os.getenv('IP', '0.0.0.0'), 
-    #         port=int(os.getenv('PORT', 4444)))
+    # app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4445)))
 
 
-audio = "./Her Majesty (Remastered 2009).wav"
+audio = "./Her_Majesty_Remastered_2009.wav"
 X = transform(audio)
 print(X.shape)
 model = tf.keras.models.load_model('./saved_model')
