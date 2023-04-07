033798fa3d32625e0d59c064f8fa5cd1e0ae4347
Simon Pino-Buisson <98675268+spynob@users.noreply.github.com>
2023-04-06 23:31:13 -0400

    Un-commeted transform method

--- app.py
+++ app.py
@@ -89,78 +89,97 @@ def predict(pred):
     
 #     return model
 
-# def transform(audioFile):
-#     # Load the audio file
-#     audio, sr = librosa.load(audioFile, sr=None)
+def transform(audioFile):
+     # Load the audio file
+     audio, sr = librosa.load(audioFile, sr=None)
 
-#     # Set the segment length and hop length in seconds
-#     segment_length = 15
-#     hop_length = 3 * sr
-#     # Calculate the number of samples in a segment
-#     segment_samples = int(segment_length * sr)
+     # Set the segment length and hop length in seconds
+     segment_length = 15
+     hop_length = 3 * sr
+     # Calculate the number of samples in a segment
+     segment_samples = int(segment_length * sr)
 
-#     # Segment the audio
-#     segments = []
-#     t1 = 0 
-#     t2 = segment_samples
-#     while t2 < len(audio):
-#         segment = audio[t1:t2]
-#         segments.append(segment)
-#         t1 = t2 - hop_length
-#         t2 = t1 + segment_samples
-#     segments.append(audio[t1:])
+     # Segment the audio
+     segments = []
+     t1 = 0 
+     t2 = segment_samples
+     while t2 < len(audio):
+         segment = audio[t1:t2]
+         segments.append(segment)
+         t1 = t2 - hop_length
+         t2 = t1 + segment_samples
+     segments.append(audio[t1:])
 
-#     print(len(segments))
-#     features = [[0 for i in range(58)] for j in segments]
-#     print("({}, {})".format(len(features), len(features[0])))
-#     for segment in range(len(segments)):
-#         features[segment][0] = len(segments[segment]) * sr
+     print(len(segments))
+     features = [[0 for i in range(58)] for j in segments]
+     print("({}, {})".format(len(features), len(features[0])))
+     for segment in range(len(segments)):
+         features[segment][0] = len(segments[segment]) * sr
 
-#         stft = librosa.feature.chroma_stft(y=segments[segment], sr=sr, hop_length=hop_length)
-#         features[segment][1] = np.mean(stft)
-#         features[segment][2] = np.var(stft)
+         stft = librosa.feature.chroma_stft(y=segments[segment], sr=sr, hop_length=hop_length)
+         features[segment][1] = np.mean(stft)
+         features[segment][2] = np.var(stft)
+         rms = librosa.feature.rms(y=segments[segment], frame_length=len(segments[segment]), hop_length=hop_length)
+         features[segment][3] = np.mean(rms)
+         features[segment][4] = np.var(rms)
 
-#         rms = librosa.feature.rms(y=segments[segment], frame_length=len(segments[segment]), hop_length=hop_length)
-#         features[segment][3] = np.mean(rms)
-#         features[segment][4] = np.var(rms)
+         spec_centroid = librosa.feature.spectral_centroid(y=segments[segment], sr=sr, hop_length=hop_length)
+         features[segment][5] = np.mean(spec_centroid)
+         features[segment][6] = np.var(spec_centroid)
 
-#         spec_centroid = librosa.feature.spectral_centroid(y=segments[segment], sr=sr, hop_length=hop_length)
-#         features[segment][5] = np.mean(spec_centroid)
-#         features[segment][6] = np.var(spec_centroid)
+         spec_bandwith = librosa.feature.spectral_bandwidth(y=segments[segment], sr=sr, hop_length=hop_length)
+         features[segment][7] = np.mean(spec_bandwith)
+         features[segment][8] = np.var(spec_bandwith)
 
-#         spec_bandwith = librosa.feature.spectral_bandwidth(y=segments[segment], sr=sr, hop_length=hop_length)
-#         features[segment][7] = np.mean(spec_bandwith)
-#         features[segment][8] = np.var(spec_bandwith)
+         rollof = librosa.feature.spectral_rolloff(y=segments[segment], sr=sr, hop_length=hop_length)
+         features[segment][9] = np.mean(rollof)
+         features[segment][10] = np.var(rollof)
 
-#         rollof = librosa.feature.spectral_rolloff(y=segments[segment], sr=sr, hop_length=hop_length)
-#         features[segment][9] = np.mean(rollof)
-#         features[segment][10] = np.var(rollof)
+         zero_cross = librosa.feature.zero_crossing_rate(y=segments[segment], frame_length=len(segments[segment]), hop_length=hop_length)
+         features[segment][11] = np.mean(zero_cross)
+         features[segment][12] = np.var(zero_cross)
+         harmony = librosa.effects.harmonic(y=segments[segment]) # WE NEED TO  LOOK AT THIS TO BE SURE IT'S THE HARMONY FEATURE
+         features[segment][13] = np.mean(harmony)
+         features[segment][14] = np.var(harmony)
 
-#         zero_cross = librosa.feature.zero_crossing_rate(y=segments[segment], frame_length=len(segments[segment]), hop_length=hop_length)
-#         features[segment][11] = np.mean(zero_cross)
-#         features[segment][12] = np.var(zero_cross)
+         percept = 0.5 #librosa.feature.tonnetz(y=segments[segment], sr=sr, hop_length=hop_length)
+         features[segment][15] = 0.5#np.mean(percept)
+         features[segment][16] = 0.5#np.var(percept)
 
-#         harmony = librosa.effects.harmonic(y=segments[segment]) # WE NEED TO  LOOK AT THIS TO BE SURE IT'S THE HARMONY FEATURE
-#         features[segment][13] = np.mean(harmony)
-#         features[segment][14] = np.var(harmony)
+         tempo = librosa.feature.tempo(y=segments[segment], sr=sr, hop_length=hop_length)
+         features[segment][17] = np.mean(tempo)
 
-#         percept = 0.5 #librosa.feature.tonnetz(y=segments[segment], sr=sr, hop_length=hop_length)
-#         features[segment][15] = 0.5#np.mean(percept)
-#         features[segment][16] = 0.5#np.var(percept)
-
-#         tempo = librosa.feature.tempo(y=segments[segment], sr=sr, hop_length=hop_length)
-#         features[segment][17] = np.mean(tempo)
-
-#         mfccs = librosa.feature.mfcc(y=segments[segment], sr=sr, n_mfcc=20, hop_length=hop_length)
-#         for i in range(0, 20):
-#             features[segment][2*i+18] = np.mean(mfccs[i])
-#             features[segment][2*i+19] = np.var(mfccs[i])
+         mfccs = librosa.feature.mfcc(y=segments[segment], sr=sr, n_mfcc=20, hop_length=hop_length)
+         for i in range(0, 20):
+             features[segment][2*i+18] = np.mean(mfccs[i])
+             features[segment][2*i+19] = np.var(mfccs[i])
 
     
-#     features0 = features[0]
+     features0 = features[0]
     
-#     return features0
+     return features0
 
+# Test run
+audio = "../Her Majesty (Remastered 2009).wav"
+#reconstructed_model = tf.keras.models.load_model("my_model_h5.h5", compile=False)
+#reconstructed_model.compile(optimizer = "adam",
+#                loss='sparse_categorical_crossentropy',
+#                metrics='accuracy')
+#audio = "/Her_Majesty_Remastered_2009.wav"
+X = transform(audio)
+model = tf.keras.models.load_model('./saved_model',compile=False)
+model.compile(optimizer = "adam",
+                loss='sparse_categorical_crossentropy',
+                metrics='accuracy')
+# print(predict(reconstructed_model, X[0]))
+# print(X)
+# reconstructed_model.predict(X)
+Xn = np.array(X)
+print(Xn.shape)
+Xn = np.tile(Xn, (128, 1))
+predict = model.predict(Xn)
+print(predict[0])
+predictIndex = np.argmax(predict, axis=1)
 # pred = predict('Data/genres_original/blues/blues.00000.wav')
 
 
